99b0a1efe89e0ee86236e2de2a3385fd31ead9eb
DenisGarifullin <64261936+DenisGarifullin@users.noreply.github.com>
2023-10-01 22:14:38 +0500

    Start repo/ add files

---
+++ birthday.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "user_birthday" (			
+	id	INTEGER,	
+	last_name	TEXT NOT NULL,	
+	first_name	TEXT NOT NULL,	
+	patronymic	TEXT NOT NULL,	
+	date_birth	TEXT NOT NULL,	
+	gender	TEXT NOT NULL,	
+	declension	INTEGER NOT NULL,	
+	PRIMARY KEY("id")		
+);			

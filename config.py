893b8f2daf8e765dc8259ce1d3154e41aa522d9f
DenisGarifullin <64261936+DenisGarifullin@users.noreply.github.com>
2023-10-02 07:38:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,1 @@
-TOKEN = '1302510932:AAHVyf4li7IYwa_004yASQyaLrIIFGewoXA' # Token BOT to BotFather
-MAIN_USER_CHAT_ID = -1001230069795 # Это ID канала церкви для Viber
-# MAIN_USER_CHAT_ID = 1031035838 # Это мой пользовательский ID
+
